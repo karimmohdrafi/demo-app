c8acc918a8d4618287e4f0d2f86a4293e6cfd5cb
karimmohdrafi <mohdrafi0701@gmail.com>
2024-03-07 22:04:39 +0530

    usestate

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-
+import React, { useState } from 'react';
 
 function App() {
-  var count = 0
-  return(
-    <>
-    <h1>Counter = {count}</h1>
-    <button onClick={update}>Change</button>
-    </>
+  const [count, setCount] = useState(0);
 
-  )
-  function update(){
-    ++count;
+  function update() {
+    setCount(count + 1);
     console.log(count);
   }
-  
-  
+
+  return (
+    <>
+      <h1>Counter = {count}</h1>
+      <button onClick={update}>Change</button>
+    </>
+  );
 }
 
 export default App;
