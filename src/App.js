be7e523ab797a89f0e95b6386dd32f333b434b0d
karimmohdrafi <mohdrafi0701@gmail.com>
2024-03-17 22:13:22 +0530

    upto destructure

--- src/App.js
+++ src/App.js
@@ -1,44 +1,69 @@
-import { useEffect, useState } from "react";
-import ProductCard from "./Product"
+// import { useEffect, useState } from "react";
+// import ProductCard from "./Product"
 
-import './App.css'
+// import './App.css'
 
+// function App() {
+//   var [products, updateProducts] = useState([])
 
+//   useEffect(
+//     () => {
+//       getProducts()
 
+//     }, []
+//   )
+//   async function getProducts() {
+//     var res = await fetch("https://fakestoreapi.com/products")
+//     let productlist = await res.json()
 
-
-function App() {
-  var [products, updateProducts] = useState([])
-
-  useEffect(
-    () => {
-      getProducts()
-
-    }, []
-  )
-  async function getProducts() {
-    var res = await fetch("https://fakestoreapi.com/products")
-    let productlist = await res.json()
-
-    updateProducts(productlist)
+//     updateProducts(productlist)
 
 
+//   }
+
+
+
+//   if (products.length == 0) {
+//     return (<h1>Fetch data</h1>)
+//   }
+//   return (
+//     <>
+//       <div className="product-list">
+//         {
+//           products.map((p) => <ProductCard {...p} key={p.id}></ProductCard>)
+
+//         }
+//       </div>
+//     </>
+//   )
+// }
+// export default App
+
+
+
+import Product from "./Product"
+
+function App(){
+  var prod1 = {
+    id: 1,
+    name: "Realme 11 Pro+",
+    aprice: 27000,
+    dmart: 30000
   }
-
-
-
-  if (products.length == 0) {
-    return (<h1>Fetch data</h1>)
+  var prod2 = {
+    id: 2,
+    name: "OnePlus 11R",
+    aprice: 47000,
+    dmart: 50000
   }
-  return (
+  
+  return(
     <>
-      <div className="product-list">
-        {
-          products.map((p) => <ProductCard {...p} key={p.id}></ProductCard>)
-
-        }
-      </div>
+    <Product {...prod1} />
+    
+    <Product {...prod2}/>
     </>
   )
 }
+ 
 export default App
--- src/Product.js
+++ src/Product.js
@@ -1,24 +1,36 @@
-function ProductCard({title,price,image,rating, description}){
+// function ProductCard({title,price,image,rating}){
 
-    // props={
-    //     name:
-    // }
+    
+
+//     return(
+
+//         <div className="card">
+
+//             <img src={image}/>
+//             <h4>{title}</h4>
+//             <center><button><p>${price}</p></button></center>
+//             <p>Rating = {rating.rate}</p>
+
+
+
+//         </div>
+
+
+//     )
+// }
+// export default ProductCard
+
+function Product({id,name,aprice,dmart}){
+
+
 
     return(
-
-        <div className="card">
-
-            <img src={image}/>
-            <h4>{title}</h4>
-            <p>{description}</p>
-            <center><button><p>${price}</p></button></center>
-            <p>Rating = {rating.rate}</p>
-
-
-
+        <div>
+            <h2>Id = {id}</h2>
+            <h2>Name = {name}</h2>
+            <h2>Cost = {aprice}</h2>
+            <h2>Dmart = {dmart}</h2>
         </div>
-
-
     )
 }
-export default ProductCard
+export default Product
